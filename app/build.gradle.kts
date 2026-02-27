2fa5a10a6b2ae81551c58043bb92b83a63b97d4a
CavidAbish <cavidabish@gmail.com>
2026-02-27 04:15:02 +0400

    Add nav_graph and bottomNav

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    //Navigation add
+    kotlin("plugin.serialization") version "2.0.21"
+    //
 }
 
 android {
@@ -37,11 +40,20 @@ android {
 
 dependencies {
 
+    //Navigation add
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    //
+    //BottomNavigation add
+    implementation("com.google.android.material:material:1.10.0")
+    //
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
