ca65cab1f64704c08fa9c7f5b64ad05a72908bd3
CavidAbish <cavidabish@gmail.com>
2026-02-28 00:09:13 +0400

    yuxaridaki bari Ui olaraq tamamladim amma funksionalliqlari daha sonra edeceyem

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     //
 
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
